61eed7251027c03eb0072e9a9e9d847eb20f5298
daisukiyo <tk.rain09@gmail.com>
2019-05-10 21:51:53 -0700

    added initial controls

--- react-redux-tetris/src/App.js
+++ react-redux-tetris/src/App.js
@@ -4,6 +4,7 @@ import GridSquare from './components/grid-square'
 import GridBoard from './components/grid-board'
 import NextBlock from './components/next-block'
 import ScoreBoard from './components/score-board'
+import Controls from './components/controls'
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <GridBoard />
         <NextBlock />
         <ScoreBoard />
+        <Controls />
       </div>
     );
   }
