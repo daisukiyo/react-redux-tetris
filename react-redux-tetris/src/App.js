adee07f9e81d6aab411039f804de42876cac2371
daisukiyo <tk.rain09@gmail.com>
2019-05-10 22:25:32 -0700

    added store and provider

--- react-redux-tetris/src/App.js
+++ react-redux-tetris/src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+
 import './App.css';
 import GridSquare from './components/grid-square'
 import GridBoard from './components/grid-board'
@@ -7,19 +11,23 @@ import ScoreBoard from './components/score-board'
 import Controls from './components/controls'
 import MessagePopup from './components/message-popup'
 
+const store = createStore(reducers)
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Tetris Redux</h1>
-        </header>
-        <GridBoard />
-        <NextBlock />
-        <ScoreBoard />
-        <Controls />
-        <MessagePopup />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Tetris Redux</h1>
+          </header>
+          <GridBoard />
+          <NextBlock />
+          <ScoreBoard />
+          <Controls />
+          <MessagePopup />
+        </div>
+      </Provider>
     );
   }
 }
