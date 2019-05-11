151e4ca7b4981d084e5e4ce171ece5302a074828
daisukiyo <tk.rain09@gmail.com>
2019-05-11 11:42:04 -0700

    added connection for score board

--- react-redux-tetris/src/components/score-board.js
+++ react-redux-tetris/src/components/score-board.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { pause, resume, restart } from '../actions'
 
 class ScoreBoard extends Component {
     render() {
+        const { isRunning, score, resume, pause, restart, gameOver } = this.props
+    
         return (
-            <div className="score-board">
-                <div>Score:{ this.props.score }</div>
-                <div>Level: 1</div>
-
-                <button className="score-board-button" onClick={(e) => {
-                
-                }}>Play</button>
-
-                <button className="score-board-button" onClick={(e) => {
-
-                }}>Restart</button>
-
-            </div>
-
-            
+          <div className="score-board">
+            <div>Score:{ score }</div>
+            <div>Level: 1</div>
+    
+            <button className="score-board-button" onClick={(e) => {
+              if (gameOver) { return }
+              isRunning ? pause() : resume()
+            }}>{isRunning ? "Pause" : "Resume"}</button>
+    
+            <button className="score-board-button" onClick={(e) => {
+            restart()
+            }}>Restart</button>
+    
+          </div>
         )
-    }
+      }
 }
 
-export default ScoreBoard
+const mapStateToProps = (state) => {
+    return {
+      score: state.game.score,
+      isRunning: state.game.isRunning,
+      gameOver: state.game.gameOver
+    }
+  }
+
+const mapDispatchToProps = () => {
+    return {
+      pause,
+      resume,
+      restart
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps())(ScoreBoard)
